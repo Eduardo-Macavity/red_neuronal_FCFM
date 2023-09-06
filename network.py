02fd2269e0fbd60dff10bcfc61e924da94f39139
Stuart Castellanos Morales <stuartcm11@gmail.com>
2023-09-05 20:34:20 -0600

    Se comentó el codigo

--- network.py
+++ network.py
@@ -19,8 +19,10 @@ import random
 # Third-party libraries
 import numpy as np
 
+# Se crea la clase network
 class Network(object):
 
+    # En sizes lleva una lista con el numero de neuronas en su respectiva capa
     def __init__(self, sizes):
         """The list ``sizes`` contains the number of neurons in the
         respective layers of the network.  For example, if the list
@@ -32,18 +34,26 @@ class Network(object):
         layer is assumed to be an input layer, and by convention we
         won't set any biases for those neurons, since biases are only
         ever used in computing the outputs from later layers."""
+
+        # Numero de capas
         self.num_layers = len(sizes)
+        # Lista con neuronas
         self.sizes = sizes
+        # Los biases y weights se generan de manera aleatoria usando numpy
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x)
                         for x, y in zip(sizes[:-1], sizes[1:])]
 
+    # Vector de activacion a_prima = (La funcion sigmoid) (wa + b)
+    # esto se hace a cada biases y weights
     def feedforward(self, a):
         """Return the output of the network if ``a`` is input."""
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w, a)+b)
         return a
 
+    # Gradiente estocastico
+    # Recibe Datos de entrenamiento, epocas, mini baches y el radio de aprendizaje respectivamente
     def SGD(self, training_data, epochs, mini_batch_size, eta,
             test_data=None):
         """Train the neural network using mini-batch stochastic
@@ -54,53 +64,67 @@ class Network(object):
         network will be evaluated against the test data after each
         epoch, and partial progress printed out.  This is useful for
         tracking progress, but slows things down substantially."""
-
+        # Se convierte en lista los datos de entrenamiento
         training_data = list(training_data)
+        # Longitud de los datos de entrenamiento
         n = len(training_data)
 
+        # Por si hay datos de prueba
         if test_data:
             test_data = list(test_data)
             n_test = len(test_data)
 
+        # Por cada epoca se elige aleatoriamente un elemento de los datos de entrenamiento
         for j in range(epochs):
             random.shuffle(training_data)
+            # Lista de los mini baches
             mini_batches = [
                 training_data[k:k+mini_batch_size]
                 for k in range(0, n, mini_batch_size)]
+            # Se actualiza cada bache
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
+            # Por si hay data de prueba
             if test_data:
                 print("Epoch {} : {} / {}".format(j,self.evaluate(test_data),n_test))
             else:
                 print("Epoch {} complete".format(j))
 
+
+    # Funion para actualizar los mini baches
+    # Se pide mini baches y el rango de aprendizaje
     def update_mini_batch(self, mini_batch, eta):
         """Update the network's weights and biases by applying
         gradient descent using backpropagation to a single mini batch.
         The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
         is the learning rate."""
+        # Se crean las matrices de ceros
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
+        # Se aplica gradiente
         for x, y in mini_batch:
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        # Se actualizan los pesos y biases
         self.weights = [w-(eta/len(mini_batch))*nw
                         for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b-(eta/len(mini_batch))*nb
                        for b, nb in zip(self.biases, nabla_b)]
 
+        # FunCion gradiente
     def backprop(self, x, y):
         """Return a tuple ``(nabla_b, nabla_w)`` representing the
         gradient for the cost function C_x.  ``nabla_b`` and
         ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
         to ``self.biases`` and ``self.weights``."""
+        # Matrices de ceros
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         # feedforward
         activation = x
-        activations = [x] # list to store all the activations, layer by layer
-        zs = [] # list to store all the z vectors, layer by layer
+        activations = [x] # lista para almacenar todas las activaciones, capa por capa
+        zs = [] # lista para almacenar todos los vectores z, capa por capa
         for b, w in zip(self.biases, self.weights):
             z = np.dot(w, activation)+b
             zs.append(z)
@@ -125,6 +149,8 @@ class Network(object):
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
         return (nabla_b, nabla_w)
 
+
+    # Funcion dar resultados de las pruebas
     def evaluate(self, test_data):
         """Return the number of test inputs for which the neural
         network outputs the correct result. Note that the neural
@@ -134,16 +160,20 @@ class Network(object):
                         for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)
 
+     # Funcion costo derivada
     def cost_derivative(self, output_activations, y):
         """Return the vector of partial derivatives \partial C_x /
         \partial a for the output activations."""
         return (output_activations-y)
 
 #### Miscellaneous functions
+
+# Funcion de activacion
 def sigmoid(z):
     """The sigmoid function."""
     return 1.0/(1.0+np.exp(-z))
 
+# Derivada de la funcion de activacion
 def sigmoid_prime(z):
     """Derivative of the sigmoid function."""
     return sigmoid(z)*(1-sigmoid(z))
